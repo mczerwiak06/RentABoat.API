2ffc3ec44f946d2fa5e0e6fa411b2a6b42e4440b
Michał Czerwiak <michal.czerwiak@gmail.com>
2022-06-29 12:10:23 +0200

    Testy jednostkowe

---
+++ RentABoat.Tests/BoatServiceTests.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using RentABoat.Core.Services;
+using RentABoat.Infrastructure.Entities;
+using RentABoat.Infrastructure.Repository;
+using Xunit;
+
+namespace RentABoat.Tests;
+
+public class BoatServiceTests
+{
+    [Fact]
+    public async Task GetBoatsAsync_ShouldReturnBoatOfGivenType()
+    {
+        var boats = new List<Boat>
+        {
+            new()
+            {
+                Type = "Motorboat",
+                Length = 12,
+                NumberOfBerths = 6,
+                YearOfBuilt = 2019,
+                Model = "SeaBreeze 550",
+                Harbour = "Sydney",
+                IsAvailable = true,
+            },
+            new()
+            {
+                Type = "Sailboat",
+                Length = 14,
+                NumberOfBerths = 8,
+                YearOfBuilt = 2020,
+                Model = "Bavaria 46 Cruiser",
+                Harbour = "Biograd",
+                IsAvailable = true,
+            }
+        };
+        var boatRepositoryMock = new Moq.Mock<IBoatRepository>();
+        boatRepositoryMock.Setup(x => x.GetAllAsync()).ReturnsAsync(boats);
+
+        var sut = new BoatService(boatRepositoryMock.Object);
+        var result = await sut.GetBoatsAsync("Sailboat");
+
+        result.Should().NotBeNull();
+        result.Where(x => x.Type == "Sailboat").Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task GetAllBoatsBasicInformationAsync_ShouldNotBeNull()
+    {
+        var boats = new List<Boat>
+        {
+            new()
+            {
+                Type = "Motorboat",
+                Length = 12,
+                NumberOfBerths = 6,
+                YearOfBuilt = 2019,
+                Model = "SeaBreeze 550",
+                Harbour = "Sydney",
+                IsAvailable = true,
+            },
+            new()
+            {
+                Type = "Sailboat",
+                Length = 14,
+                NumberOfBerths = 8,
+                YearOfBuilt = 2020,
+                Model = "Bavaria 46 Cruiser",
+                Harbour = "Biograd",
+                IsAvailable = true,
+            }
+        };
+        var boatRepositoryMock = new Moq.Mock<IBoatRepository>();
+        boatRepositoryMock.Setup(x => x.GetAllAsync()).ReturnsAsync(boats);
+
+        var sut = new BoatService(boatRepositoryMock.Object);
+        var result = await sut.GetAllBoatsBasicInformationAsync();
+        result.Should().NotBeNull();
+        result.Count().Should().Be(2);
+    }
+}
---
+++ RentABoat.Tests/SailorAccountServiceTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using RentABoat.Core.Services;
+using RentABoat.Infrastructure.Entities;
+using RentABoat.Infrastructure.Repository;
+using Xunit;
+
+namespace RentABoat.Tests;
+
+public class SailorAccountServiceTests
+{
+    [Fact]
+    public async Task GetAllSailorsBasicInformationAsync_ShouldNotBeNull()
+    {
+        var sailors = new List<SailorAccount>
+        {
+            new()
+            {
+                FirstName = "Adam",
+                LastName = "Nowak",
+                Email = "nowak@mail.com",
+                PhoneNumber = "555-555-555",
+                Street = "Laskowa",
+                City = "Gdynia",
+                ZipCode = "81-103",
+                BuildingNumber = 24,
+            },
+            new()
+            {
+                FirstName = "Rafał",
+                LastName = "Kata",
+                Email = "katak@mail.com",
+                PhoneNumber = "444-444-444",
+                Street = "Polna",
+                City = "Gdynia",
+                ZipCode = "81-103",
+                BuildingNumber = 28,
+            },
+            new()
+            {
+                FirstName = "Ewa",
+                LastName = "Równa",
+                Email = "erownak@mail.com",
+                PhoneNumber = "222-222-222",
+                Street = "Kwiatowa",
+                City = "Gdynia",
+                ZipCode = "81-103",
+                BuildingNumber = 212,
+            },
+        };
+        var sailorAccountRepositoryMock = new Moq.Mock<ISailorAccountRepository>();
+        sailorAccountRepositoryMock.Setup(x => x.GetAllAsync()).ReturnsAsync(sailors);
+
+        var sut = new SailorAccountService(sailorAccountRepositoryMock.Object);
+
+        var result = await sut.GetAllSailorsBasicInformationAsync();
+        result.Should().NotBeNull();
+        result.Count().Should().Be(3);
+    }
+}
